7d4e6d24856928744c8e89c9da331d5033657136
Jacob Chademwiri <jchademwiri@gmail.com>
2023-06-30 16:36:16 +0200

    v1 SEO Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,54 @@ import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: {
+    default: 'Jacob chademwiri Web Developer in Centurion',
+    template: '%s | Web Developer in Centurion',
+  },
+  description:
+    'I am a Frontend Developer in Centurion, SA. I love to build websites  with JavaScript, Next.js, React, Express, Tailwind and MongoDB.',
 
+  openGraph: {
+    title: 'Frontend Developer in Centurion, SA',
+    description: `I'm a frontend developer in Centurion, SA. I love to build websites  with JavaScript, Next.js, React, Express, Tailwind and MongoDB.`,
+    url: `https://jacobc.co.za`,
+    siteName: 'Jacob Chademwiri',
+    images: [
+      {
+        url: `https://jacobc.co.za/social.jpg`,
+        width: 1200,
+        height: 600,
+        alt: 'Jacob Chademwiri Frontend Developer in Centurion, SA',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  category: 'technology',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jacob Chademwiri',
+    description: `I'm a frontend developer in Centurion, SA. I love to build websites  with JavaScript, Next.js, React, Express, Tailwind and MongoDB.`,
+    siteId: '1467726470533754880',
+    creator: '@jchademwiri',
+    creatorId: '1467726470533754880',
+    images: ['https://jacobc.co.za/social.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
