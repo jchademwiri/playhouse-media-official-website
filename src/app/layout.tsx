15ffa5c5c23467aedb060aaaf3eaf4409827da10
Jacob Chademwiri <jchademwiri@gmail.com>
2023-07-13 16:45:58 +0200

    meta type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-import { Metadata } from 'next';
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] });
 
