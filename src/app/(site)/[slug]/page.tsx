614ec31e9b346367447ddf936490786b8c1aedf3
Jacob Chademwiri <jchademwiri@gmail.com>
2023-07-25 09:50:53 +0200

    post slug page

--- src/app/(site)/[slug]/page.tsx
+++ src/app/(site)/[slug]/page.tsx
@@ -1,8 +1,5 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { PortableText } from '@portabletext/react';
-import { MARKS, } from '@contentful/rich-text-types';
 import Link from 'next/link';
-import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { getPost } from '@/lib/sanity/sanity-utils';
 
@@ -23,23 +20,11 @@ export async function generateMetadata({
 
 const PostSlug = async ({ params: { slug } }: Props) => {
   const post = await getPost(slug);
-
-  const options = {
-    renderMark: {
-      [MARKS.CODE]: (text: ReactNode) => (
-        <pre>
-          <code>{text}</code>
-        </pre>
-      ),
-    },
-  };
-
   return (
     <section className='mx-auto my-20 w-full max-w-[1240px] px-4'>
       <Link href='/blog'>Back to Blog</Link>
       <h1>{post.title}</h1>
       <div className='prose max-w-[1240px] dark:prose-invert'>
-        {/* {documentToReactComponents(post.content, options)} */}
         <PortableText value={post.content} />
       </div>
     </section>
