4bedaab5434bd2e9a6610aa97dfdd41c6f7fa640
Jacob Chademwiri <jchademwiri@gmail.com>
2023-10-27 11:19:48 +0200

    title mobile text size

--- src/components/ui/TitleHeader.tsx
+++ src/components/ui/TitleHeader.tsx
@@ -7,7 +7,7 @@ const TitleHeader = ({ title, subTitle }: TitleHeader) => {
   return (
     <div className='items-end justify-between md:flex'>
       <div>
-        <h2 className='text-4xl font-semibold md:text-6xl'>{title}</h2>
+        <h2 className='text-3xl font-semibold md:text-6xl'>{title}</h2>
         <div className='flex items-center gap-2 py-4'>
           <span className='h-1 w-20 rounded-full  bg-primary'></span>
           <h2 className='text-2xl font-medium text-primary'>{subTitle}</h2>
