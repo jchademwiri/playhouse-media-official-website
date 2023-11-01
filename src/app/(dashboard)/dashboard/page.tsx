1712c9d5cbd54164036637912937db7a34e4038e
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-01 13:12:24 +0200

    updated nav

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,12 +3,8 @@ const Dashboard = () => {
     <div>
       <>
         <span className='font-bold text-4xl'>Home</span>
-        <div className='border-dashed border border-zinc-500 w-full h-12 rounded-lg'></div>
-        <div className='border-dashed border border-zinc-500 w-full h-64 rounded-lg'></div>
-        <div className='border-dashed border border-zinc-500 w-full h-64 rounded-lg'></div>
-        <div className='border-dashed border border-zinc-500 w-full h-64 rounded-lg'></div>
-        <div className='border-dashed border border-zinc-500 w-full h-64 rounded-lg'></div>
-        <div className='border-dashed border border-zinc-500 w-full h-64 rounded-lg'></div>
+        <div className='border-dashed border border-secondary w-full h-12 rounded-lg'></div>
+        <div className='border-dashed border border-secondary w-full h-64 rounded-lg'></div>
       </>
     </div>
   );
