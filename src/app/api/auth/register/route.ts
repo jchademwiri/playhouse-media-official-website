9450fecb91d6ce49bb25a5c4266fad977344309b
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-07 10:57:15 +0200

    Create user API route

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,24 +1,43 @@
-import { RegisterForm, RegisterFormSchema } from '@/lib/models';
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+import { hash } from 'bcrypt';
 
 export async function POST(request: NextRequest) {
-  console.log('first');
-  const body = await request.json();
-  const validation = RegisterFormSchema.safeParse(body);
+  try {
+    const body = await request.json();
+    const { fullname, email, password } = body;
 
-  if (!validation.success) {
-    return NextResponse.json(validation.error.format(), { status: 400 });
+    const existingUserByEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    // check if user exists
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        { user: null, message: 'Email already exists' },
+        { status: 409 }
+      );
+    }
+    const hashedPassword = await hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        fullname,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        Newuser: newUser,
+        message: 'New User has been created successfully',
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
   }
-
-  const newUser = await prisma.user.create({
-    data: {
-      fullname: body.fullname,
-      email: body.email,
-      password: body.password,
-    },
-  });
-  return NextResponse.json(newUser, { status: 201 });
 }
 
 export async function GET() {
