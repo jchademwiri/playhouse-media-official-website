693ac3d3dd5671b6c4fc96a83a6334220619815f
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-08 05:40:30 +0200

    auth complete

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,29 +1,27 @@
+import User from '@/components/User';
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session);
-
-  if (session?.user) {
-    return (
-      <section>
-        <>
-          <span className='font-bold text-4xl'>
-            Welcome {session?.user.fullname}
-          </span>
-
-          <div className='border-dashed border border-secondary w-full h-12 rounded-lg'></div>
-          <div className='border-dashed border border-secondary w-full h-64 rounded-lg'></div>
-        </>
-      </section>
-    );
-  }
 
   return (
-    <div>
-      <span className='font-bold text-4xl'>Please Login to access</span>
-    </div>
+    <section>
+      <>
+        <span className='font-bold text-4xl'>
+          Welcome {session?.user.fullname}
+        </span>
+
+        <div className='border-dashed border border-secondary w-full rounded-lg'>
+          <h2>Client Session:</h2>
+          <User />
+        </div>
+        <div className='border-dashed border border-secondary w-full rounded-lg'>
+          <h2>Server Session:</h2>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
+      </>
+    </section>
   );
 };
 export default Dashboard;
