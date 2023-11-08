ba817364f2457d93e0aaecc38966fc6afdcf1ff1
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-08 07:27:33 +0200

    swithched database

--- src/prisma/migrations/20231107083007_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `updatedAt` to the `user` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
