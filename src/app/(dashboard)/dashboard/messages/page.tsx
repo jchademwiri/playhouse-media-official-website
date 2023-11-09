7bee1c7313c81c9f36332a34ee1d7d8cefab53db
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-09 15:20:45 +0200

    conditional message rendering

--- src/app/(dashboard)/dashboard/messages/page.tsx
+++ src/app/(dashboard)/dashboard/messages/page.tsx
@@ -12,7 +12,11 @@ const ContactMessages = async () => {
     <section>
       <span className='font-bold text-4xl'>Messages:</span>
       <div className='border-dashed  p-2 border border-zinc-500 w-full min-h-64 rounded-lg'>
-        <pre>{JSON.stringify(messages, null, 2)}</pre>
+        {messages.length === 0 ? (
+          <p className='font-bold text-xl'>No Message Yet</p>
+        ) : (
+          <pre>{JSON.stringify(messages, null, 2)}</pre>
+        )}
       </div>
     </section>
   );
