e62d8dbb8b33a221532f541b6a483cc252554d2f
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-09 09:18:26 +0200

    padding on messages

--- src/app/(dashboard)/dashboard/messages/page.tsx
+++ src/app/(dashboard)/dashboard/messages/page.tsx
@@ -6,7 +6,7 @@ const Messages = async () => {
     <>
       <span className='font-bold text-4xl'>Messages</span>
 
-      <div className='border-dashed border border-zinc-500 w-full min-h-64 rounded-lg'>
+      <div className='border-dashed  p-2 border border-zinc-500 w-full min-h-64 rounded-lg'>
         <pre>{JSON.stringify(messages, null, 2)}</pre>
       </div>
     </>
