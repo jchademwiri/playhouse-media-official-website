b9d30aa0972d9feab5f5bbb17e846c9de30d40f0
Jacob Chademwiri <jchademwiri@gmail.com>
2023-11-10 08:39:28 +0200

    revalidate and sorting

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -2,6 +2,10 @@ import prisma from '@/prisma/client';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const messages = await prisma.message.findMany();
+  const messages = await prisma.message.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
   return new NextResponse(JSON.stringify(messages), { status: 200 });
 }
--- src/data/fetch/index.ts
+++ src/data/fetch/index.ts
@@ -1,7 +1,9 @@
 import { server } from '@/config';
 
 export async function getMessages() {
-  const res = await fetch(`${server}/api/messages`);
+  const res = await fetch(`${server}/api/messages`, {
+    next: { revalidate: 10 },
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
