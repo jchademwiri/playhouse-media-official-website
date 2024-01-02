8b27ae9019f907ecdf9cf3a51d615d1f72c813db
Jacob Chademwiri <jchademwiri@gmail.com>
2024-01-02 17:38:51 +0200

    added Remote GithubRepo: Custom Image and Video Component

---
+++ src/lib/posts.ts
@@ -0,0 +1,124 @@
+import { compileMDX } from 'next-mdx-remote/rsc';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings/lib';
+import rehypeHighlight from 'rehype-highlight/lib';
+import rehypeSlug from 'rehype-slug';
+import Video from '@/components/Video';
+import CustomImage from '@/components/CustomImage';
+
+type Filetree = {
+  tree: [
+    {
+      path: string;
+    },
+  ];
+};
+
+const rawUrl =
+  'https://raw.githubusercontent.com/jchademwiri/pmg-blog/master/posts';
+
+const apiUrl =
+  'https://api.github.com/repos/jchademwiri/pmg-blog/git/trees/master?recursive=1';
+
+export async function getPostByName(
+  fileName: string
+): Promise<BlogPost | undefined> {
+  const res = await fetch(
+    `${rawUrl}/${fileName}`,
+
+    // https://raw.githubusercontent.com/gitdagray/test-blogposts/main/${fileName}
+    {
+      headers: {
+        accept: 'application/vnd.github+json',
+        Authorization: `Bearer  ${process.env.GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    }
+  );
+  if (!res.ok) return undefined;
+
+  const rawMDX = await res.text();
+
+  if (rawMDX === '404: Not Found') return undefined;
+
+  const { frontmatter, content } = await compileMDX<{
+    title: string;
+    excerpt: string;
+    date: string;
+    tags: string[];
+  }>({
+    source: rawMDX,
+    components: {
+      Video,
+      CustomImage,
+    },
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        rehypePlugins: [
+          rehypeHighlight,
+          rehypeSlug,
+          [
+            rehypeAutolinkHeadings,
+            {
+              behavior: 'wrap',
+            },
+          ],
+        ],
+      },
+    },
+  });
+
+  const id = fileName.replace(/\.mdx$/, '');
+
+  const blogPostObj: BlogPost = {
+    meta: {
+      id,
+      title: frontmatter.title,
+      excerpt: frontmatter.excerpt,
+      date: frontmatter.date,
+      tags: frontmatter.tags,
+    },
+    content,
+  };
+
+  return blogPostObj;
+}
+
+export async function getPostsMeta(): Promise<Meta[] | undefined> {
+  const res = await fetch(`${apiUrl}`, {
+    headers: {
+      accept: 'application/vnd.github+json',
+      Authorization: `Bearer  ${process.env.GITHUB_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+  if (!res.ok) return undefined;
+
+  const repoFiletree: Filetree = await res.json();
+
+  const filesArray = repoFiletree.tree
+    .map((obj) => obj.path)
+    .filter((path) => path.endsWith('.mdx'));
+
+  const posts: Meta[] = [];
+
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) {
+      const { meta } = post;
+      posts.push(meta);
+    }
+  }
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+function compileMDX<T>(arg0: {
+  source: string;
+  components: { Video: any; CustomImage: any };
+  options: { parseFrontmatter: boolean; mdxOptions: { rehypePlugins: any[] } };
+}):
+  | { frontmatter: any; content: any }
+  | PromiseLike<{ frontmatter: any; content: any }> {
+  throw new Error('Function not implemented.');
+}
+// https://youtu.be/6ih_3m_UPKg?t=1126
--- types.d.ts
+++ types.d.ts
@@ -27,9 +27,15 @@ type Post = {
   };
 };
 
-type BlogPost = {
+type Meta = {
   id: string;
   title: string;
-  date: string;
   excerpt: string;
+  date: string;
+  tags: string[];
+};
+
+type BlogPost = {
+  meta: Meta;
+  content: ReactElement<any, string | JSXElementConstructor<any>>;
 };
