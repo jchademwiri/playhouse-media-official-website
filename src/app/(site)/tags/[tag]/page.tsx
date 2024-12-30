7e6e3adcfb684c8260609a709d8ae6d68c6fccd5
Jacob Chademwiri <jchademwiri@gmail.com>
2024-12-30 12:58:03 +0200

    refactor: update import quotes and set revalidate to 0

--- src/app/(site)/tags/[tag]/page.tsx
+++ src/app/(site)/tags/[tag]/page.tsx
@@ -1,9 +1,9 @@
-import PostCard from '@/components/PostCard';
-import { getPostsMeta } from '@/lib/posts';
+import PostCard from "@/components/PostCard";
+import { getPostsMeta } from "@/lib/posts";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-export const revalidate = 3600;
+export const revalidate = 0;
 
 type Props = {
   params: {
@@ -32,13 +32,13 @@ export function generateMetadata({ params: { tag } }: Props) {
     openGraph: {
       title: `Posts about ${tag}`,
       description: `Welcome to Playhouse Media Group Blog Posts about ${tag}, these posts will teach you about ${tag}, learn something new everyday.`,
-      images: '/opengraph-image.png',
+      images: "/opengraph-image.png",
     },
     twitter: {
       title: `Posts about ${tag}`,
       description: `Welcome to Playhouse Media Group Blog Posts about ${tag}, these posts will teach you about ${tag}, learn something new everyday.`,
     },
-    images: '/twitter-image.png',
+    images: "/twitter-image.png",
   };
 }
 
@@ -46,26 +46,26 @@ export default async function TagPostList({ params: { tag } }: Props) {
   const posts = await getPostsMeta(); //deduped!
 
   if (!posts)
-    return <p className='mt-10 text-center'>Sorry, no posts available.</p>;
+    return <p className="mt-10 text-center">Sorry, no posts available.</p>;
 
   const tagPosts = posts.filter((post) => post.tags.includes(tag));
 
   if (!tagPosts.length) {
     return (
-      <div className='text-center'>
-        <p className='mt-10'>Sorry, no posts for that keyword.</p>
-        <Link href='/blog'>Back to Blog</Link>
+      <div className="text-center">
+        <p className="mt-10">Sorry, no posts for that keyword.</p>
+        <Link href="/blog">Back to Blog</Link>
       </div>
     );
   }
 
   return (
-    <section className='mx-auto my-10 w-full max-w-[1240px] px-4'>
-      <h1 className=' text-2xl font-semibold md:font-bold md:text-4xl my-8'>
+    <section className="mx-auto my-10 w-full max-w-[1240px] px-4">
+      <h1 className=" text-2xl font-semibold md:font-bold md:text-4xl my-8">
         Results for: #{tag}
       </h1>
-      <section className='grid gap-4'>
-        <section className='w-full grid sm:grid-cols-2 lg:grid-cols-3  gap-4'>
+      <section className="grid gap-4">
+        <section className="w-full grid sm:grid-cols-2 lg:grid-cols-3  gap-4">
           {tagPosts.map((post: Meta) => (
             <PostCard key={post.id} {...post} />
           ))}
