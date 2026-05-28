45420493cd05322467ce7fa905c741dfaaf6b8df
Jacob Chademwiri <jchademwiri@gmail.com>
2026-05-28 03:41:54 +0200

    Fix sitemap domain override and bump next-mdx-remote to v6

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
+const canonicalSiteUrl = 'https://www.playhousemedia.co.za';
+const envSiteUrl = process.env.SITE_URL;
+
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.playhousemedia.co.za',
+  siteUrl:
+    envSiteUrl && envSiteUrl.includes('playhousemedia.co.za')
+      ? envSiteUrl
+      : canonicalSiteUrl,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/dashboard/*', '/admin/*', '/api/*'],
